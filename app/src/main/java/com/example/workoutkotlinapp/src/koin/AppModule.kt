0460677b03dcf4d12800d5231292aa0c01649d8d
lidordvir <lidor4001@gmail.com>
2024-02-05 10:33:30 +0200

    * Register Screen UI

--- app/src/main/java/com/example/workoutkotlinapp/src/koin/AppModule.kt
+++ app/src/main/java/com/example/workoutkotlinapp/src/koin/AppModule.kt
@@ -1,10 +1,14 @@
 package com.example.workoutkotlinapp.src.koin
 
 import com.example.workoutkotlinapp.src.screens.Login.LoginViewModel
+import com.example.workoutkotlinapp.src.screens.Register.RegisterViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule =
     module {
-        viewModel { LoginViewModel() }
+        viewModel {
+            LoginViewModel()
+            RegisterViewModel()
+        }
     }
