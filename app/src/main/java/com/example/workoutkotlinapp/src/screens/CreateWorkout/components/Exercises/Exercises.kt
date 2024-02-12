def898ce35f736ec90ce62f6cb096135f20a29a0
lidordvir <lidor4001@gmail.com>
2024-02-12 15:15:30 +0200

    * WIP Refactor create workout screen

--- app/src/main/java/com/example/workoutkotlinapp/src/screens/CreateWorkout/components/Exercises/Exercises.kt
+++ app/src/main/java/com/example/workoutkotlinapp/src/screens/CreateWorkout/components/Exercises/Exercises.kt
@@ -1,13 +1,13 @@
 package com.example.workoutkotlinapp.src.screens.CreateWorkout.components.Exercises
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.lifecycle.map
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.workoutkotlinapp.src.screens.CreateWorkout.CreateWorkoutViewModel
+import com.example.workoutkotlinapp.src.screens.CreateWorkout.components.Exercises.ExerciseItem.ExerciseItem
 
 @Composable
 fun Exercises() {
@@ -18,7 +18,7 @@ fun Exercises() {
     if (exercises !== null) {
         Column {
             exercises!!.forEachIndexed { index, exercise ->
-                Text(text = "Exercise num ${index + 1}")
+                ExerciseItem(exercise)
             }
         }
     }
