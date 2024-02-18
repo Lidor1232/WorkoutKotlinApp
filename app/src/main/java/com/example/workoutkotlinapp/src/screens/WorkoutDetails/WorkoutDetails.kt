44885f03e81c139d3abb74887d09bc563acfdb9b
lidordvir <lidor4001@gmail.com>
2024-02-18 13:19:32 +0200

    * Reset to workout details screen on unmount

--- app/src/main/java/com/example/workoutkotlinapp/src/screens/WorkoutDetails/WorkoutDetails.kt
+++ app/src/main/java/com/example/workoutkotlinapp/src/screens/WorkoutDetails/WorkoutDetails.kt
@@ -2,12 +2,14 @@ package com.example.workoutkotlinapp.src.screens.WorkoutDetails
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.lifecycle.map
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.workoutkotlinapp.MainViewModel
+import com.example.workoutkotlinapp.src.screens.Login.LoginIntent
 import com.example.workoutkotlinapp.src.screens.WorkoutDetails.components.BackButton.BackButton
 import com.example.workoutkotlinapp.src.screens.WorkoutDetails.components.ErrorHandler.ErrorHandler
 import com.example.workoutkotlinapp.src.screens.WorkoutDetails.components.Exercises.Exercises
@@ -21,6 +23,12 @@ fun WorkoutDetails() {
 
     val token by mainViewModel.state.map { it.token }.observeAsState()
     val workoutId by workoutDetailsViewModel.state.map { it.workout?._id }.observeAsState()
+
+    DisposableEffect(Unit) {
+        onDispose {
+            workoutDetailsViewModel.processIntent(WorkoutDetailsIntent.Reset)
+        }
+    }
 
     LaunchedEffect(workoutDetailsViewModel) {
         if (token != null && workoutId != null) {
