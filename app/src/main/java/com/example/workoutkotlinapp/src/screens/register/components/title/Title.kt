29ef87bbe714db3ea3ca3097b52054579f1f42cc
lidordvir <lidor4001@gmail.com>
2024-02-29 11:19:27 +0200

    * Add strings register screen

--- app/src/main/java/com/example/workoutkotlinapp/src/screens/register/components/title/Title.kt
+++ app/src/main/java/com/example/workoutkotlinapp/src/screens/register/components/title/Title.kt
@@ -1,11 +1,13 @@
 package com.example.workoutkotlinapp.src.screens.register.components.title
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.workoutkotlinapp.R
 import com.example.workoutkotlinapp.src.components.Titles.ScreenTitle.ScreenTitle
 
 @Composable
 fun Title() {
     ScreenTitle(
-        text = "Register Screen",
+        text = stringResource(R.string.register_screen_title),
     )
 }
