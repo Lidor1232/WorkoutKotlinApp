93c388642fb010398f734645747d7fbed300d785
lidordvir <lidor4001@gmail.com>
2024-03-04 09:30:29 +0200

    * Refactoring

--- app/src/main/java/com/example/workoutkotlinapp/src/screens/createExercise/CreateExerciseModel.kt
+++ app/src/main/java/com/example/workoutkotlinapp/src/screens/createExercise/CreateExerciseModel.kt
@@ -1,19 +1,19 @@
 package com.example.workoutkotlinapp.src.screens.createExercise
 
-import com.example.workoutkotlinapp.src.types.ExerciseSet
+import com.example.workoutkotlinapp.src.types.Exercise
 
 data class AddSetState(val reps: String, val weight: String)
 
 data class CreateExerciseState(
     val name: String = "",
-    val sets: List<ExerciseSet> = listOf(),
+    val sets: List<Exercise.Set> = listOf(),
     val addSet: AddSetState = AddSetState(reps = "", weight = ""),
 )
 
 sealed class CreateExerciseIntent() {
     data class SetName(val name: String) : CreateExerciseIntent()
 
-    data class AddSet(val set: ExerciseSet) : CreateExerciseIntent()
+    data class AddSet(val set: Exercise.Set) : CreateExerciseIntent()
 
     data class AddSetUpdateReps(val reps: String) : CreateExerciseIntent()
 
--- app/src/main/java/com/example/workoutkotlinapp/src/types/Exercise.kt
+++ app/src/main/java/com/example/workoutkotlinapp/src/types/Exercise.kt
@@ -1,3 +1,5 @@
 package com.example.workoutkotlinapp.src.types
 
-data class Exercise(val id: String, val name: String, val workout: String, val sets: List<ExerciseSet>)
+data class Exercise(val id: String, val name: String, val workout: String, val sets: List<Set>) {
+    data class Set(val weight: Int, val reps: Int)
+}
